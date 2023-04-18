f65ed11d210c9b6f2f14b5436ec80906046d1ff0
deckerayers <ayersdecker@gmail.com>
2023-04-18 14:28:37 -0400

    Success

---
+++ UsingClasses/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog("Fido", 12);
+        System.out.println("The dog's name is " + dog.getName() + ", and his age is " + dog.getAge());
+
+
+
+    }
+}
